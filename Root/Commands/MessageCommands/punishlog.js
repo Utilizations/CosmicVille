cc3bbdab2c9da961786f546b69f910f4d1ac0c91
Utilizations <Utilizationsbusiness@gmail.com>
2022-07-24 23:12:15 +0100

    dw

---
+++ Root/Commands/MessageCommands/punishlog.js
@@ -0,0 +1,10 @@
+const Discord = require("discord.js");
+const config = require("../../../Config");
+
+module.exports = {
+    name: "punishlog",
+    run: async(client, message, args, container) => {
+        message.delete()
+
+    }
+}
