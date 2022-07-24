30b6262b03d4bc2f0a37916f492700aaf9a43f1d
Utilizations <Utilizationsbusiness@gmail.com>
2022-07-24 23:15:01 +0100

    dwdwa

--- Root/Commands/MessageCommands/info.js
+++ Root/Commands/MessageCommands/info.js
@@ -5,6 +5,16 @@ module.exports = {
     name: "info",
     run: async(client, message, args, container) => {
         message.delete()
+        const embed = new Discord.MessageEmbed()
+        .setTitle("Post your Info")
+        .setDescription("Upon joining CosmicVille staff we request you to fill out this form so we have your detail on hand. Please fill out the form by clicking the button bellow.")
 
+        const row = new Discord.MessageActionRow().addComponents(
+            new Discord.MessageButton()
+            .setCustomId('info')
+            .setLabel('Post Info.')
+            .setStyle('SUCCESS')
+        )
+        message.channel.send({embeds: [embed], components: [row]})
     }
 }
--- Root/Commands/MessageCommands/punishlog.js
+++ Root/Commands/MessageCommands/punishlog.js
@@ -5,6 +5,16 @@ module.exports = {
     name: "punishlog",
     run: async(client, message, args, container) => {
         message.delete()
+        const embed = new Discord.MessageEmbed()
+        .setTitle("Post a Punishment Log.")
+        .setDescription("If you have just punished a player in-game please fill out the form by clicking the button bellow.")
 
+        const row = new Discord.MessageActionRow().addComponents(
+            new Discord.MessageButton()
+            .setCustomId('punishment')
+            .setLabel('Post a punishment.')
+            .setStyle('DANGER')
+        )
+        message.channel.send({embeds: [embed], components: [row]})
     }
 }
